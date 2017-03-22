47ee10429ea2cf5cd2ff16204958d8c89ee86490
Deen Ma <deenma@Deens-MacBook-Pro.local>
2017-03-23 01:25:39 +0800

    Mar. 23: Figured out the way to take picture and save to external storage - should be one of the most challenging part of the app TODO: 1. create an activity to display the taken pictures 2. for continue button, make sure 5 required pictures got taken and then make it clickable

--- app/src/main/java/com/example/deenma/chef/Constants.java
+++ app/src/main/java/com/example/deenma/chef/Constants.java
@@ -5,5 +5,18 @@ package com.example.deenma.chef;
  */
 
 public class Constants {
+    // delay length
     public static final Long DELAY_WELCOME_ACTIVITY = 3000L;
+
+    // request code
+    public static final int REQUEST_IMAGE_CAPTURE = 1;
+
+    // file names
+    public static final String APP_NAME = "chef";
+    public static final String IMAGE_FRONT_SIDE_VIEW = "front_side_view";
+    public static final String IMAGE_COLLISION_YOUR_CAR = "collision_your_car";
+    public static final String IMAGE_COLLISION_OPPONENTS_CAR = "collision_opponents_car";
+    public static final String LICENSE_PLATE_YOUR_CAR = "license_plate_your_car";
+    public static final String LICENSE_PLATE_OPPONENTS_CAR = "license_plate_opponents_car";
+    public static final String ADDITIONAL = "additional";
 }
--- app/src/main/java/com/example/deenma/chef/activities/TakePictureIIActivity.java
+++ app/src/main/java/com/example/deenma/chef/activities/TakePictureIIActivity.java
@@ -1,9 +1,17 @@
 package com.example.deenma.chef.activities;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.deenma.chef.R;
+import java.io.File;
+import com.example.deenma.chef.Constants;
 
 /**
  * Created by deenma on 19/03/2017.
@@ -14,5 +22,140 @@ public class TakePictureIIActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_take_picture_ii);
+        setButtons();
     }
+
+    private void setButtons() {
+        Button buttonFrontSideView = (Button) findViewById(R.id.take_picture_ii_button_front_side_view);
+        buttonFrontSideView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                viewImage(Constants.IMAGE_FRONT_SIDE_VIEW); // name of the image
+            }
+        });
+
+        Button buttonFrontSideViewTake = (Button) findViewById(R.id.take_picture_ii_button_front_side_view_take);
+        buttonFrontSideViewTake.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                takePictureAndSave(Constants.IMAGE_FRONT_SIDE_VIEW);
+            }
+        });
+
+        Button buttonCollisionYourCar = (Button) findViewById(R.id.take_picture_ii_button_collision_your_car);
+        buttonCollisionYourCar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                viewImage(Constants.IMAGE_COLLISION_YOUR_CAR);
+            }
+        });
+
+        Button buttonCollisionYourCarTake = (Button) findViewById(R.id.take_picture_ii_button_collision_your_car_take);
+        buttonCollisionYourCarTake.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                takePictureAndSave(Constants.IMAGE_COLLISION_YOUR_CAR);
+            }
+        });
+
+        Button buttonCollisionOpponentsCar = (Button) findViewById(R.id.take_picture_ii_button_collision_opponents_car);
+        buttonCollisionOpponentsCar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                viewImage(Constants.IMAGE_COLLISION_OPPONENTS_CAR);
+            }
+        });
+
+        Button buttonCollisionOpponentsCarTake =
+                (Button) findViewById(R.id.take_picture_ii_button_collision_opponents_car_take);
+        buttonCollisionOpponentsCarTake.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                takePictureAndSave(Constants.IMAGE_COLLISION_OPPONENTS_CAR);
+            }
+        });
+
+        Button buttonLicensePlateYourCar = (Button) findViewById(R.id.take_picture_ii_button_license_plate_your_car);
+        buttonLicensePlateYourCar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                viewImage(Constants.LICENSE_PLATE_YOUR_CAR);
+            }
+        });
+
+        Button buttonLicensePlateYourCarTake =
+                (Button) findViewById(R.id.take_picture_ii_button_license_plate_your_car_take);
+        buttonLicensePlateYourCarTake.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                takePictureAndSave(Constants.LICENSE_PLATE_YOUR_CAR);
+            }
+        });
+
+        Button buttonLicensePlateOpponentsCar =
+                (Button) findViewById(R.id.take_picture_ii_button_license_plate_opponents_car);
+        buttonLicensePlateOpponentsCar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                viewImage(Constants.LICENSE_PLATE_OPPONENTS_CAR);
+            }
+        });
+
+        Button buttonLicensePlateOpponentsCarTake =
+                (Button) findViewById(R.id.take_picture_ii_button_license_plate_opponents_car_take);
+        buttonLicensePlateOpponentsCarTake.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                takePictureAndSave(Constants.LICENSE_PLATE_OPPONENTS_CAR);
+            }
+        });
+
+        // button_additional
+        Button buttonAdditional =
+                (Button) findViewById(R.id.take_picture_ii_button_additional);
+        buttonAdditional.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                viewImage(Constants.ADDITIONAL);
+            }
+        });
+
+        Button buttonAdditionalTake =
+                (Button) findViewById(R.id.take_picture_ii_button_additional_take);
+        buttonAdditionalTake.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                takePictureAndSave(Constants.ADDITIONAL);
+            }
+        });
+    }
+
+    private void takePictureAndSave(String filename) {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        Uri uri = Uri.fromFile(getOutputMediaFile(filename));
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, uri);
+        startActivityForResult(intent, Constants.REQUEST_IMAGE_CAPTURE);
+    }
+
+    private File getOutputMediaFile(String filename) {
+        File mediaStorageDir = new File(Environment.getExternalStoragePublicDirectory(
+                Environment.DIRECTORY_PICTURES), Constants.APP_NAME);
+        if (!mediaStorageDir.exists()) {
+            if (!mediaStorageDir.mkdirs()) {
+                return null;
+            }
+        }
+        String photoPath = mediaStorageDir.getPath() + File.separator +
+                "IMG_"+ filename + ".jpg";
+        return new File(photoPath);
+    }
+
+    private void viewImage(String filename) {
+        // create an intent and putExtra the filename, then open this file referring to viewPicture()
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+    }
+
 }
