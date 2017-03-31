cb6acb3223869b9fdf4f22f0962f29d87c05815c
Deen Ma <deenma@Deens-MacBook-Pro.local>
2017-03-31 14:13:07 +0800

    Mar. 28: Continue to Information Acticity

---
+++ app/src/main/java/com/example/deenma/chef/activities/InformationActivity.java
@@ -0,0 +1,18 @@
+package com.example.deenma.chef.activities;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.example.deenma.chef.R;
+
+/**
+ * Created by deenma on 29/03/2017.
+ */
+
+public class InformationActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_information);
+    }
+}
--- app/src/main/java/com/example/deenma/chef/activities/TakePictureIIIActivity.java
+++ app/src/main/java/com/example/deenma/chef/activities/TakePictureIIIActivity.java
@@ -1,7 +1,11 @@
 package com.example.deenma.chef.activities;
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.deenma.chef.R;
 
@@ -14,7 +18,14 @@ public class TakePictureIIIActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_take_picture_iii);
+        Button buttonContinue = (Button) findViewById(R.id.steps_button_continue);
+        final Context ctx = this;
+        buttonContinue.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ctx, InformationActivity.class);
+                startActivity(intent);
+            }
+        });
     }
-
-    // TODO: define button continue
 }
