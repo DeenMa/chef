2823c8a82302210124e4f091e4b0876b797c2529
Deen Ma <deenma@Deens-MacBook-Pro.local>
2017-04-01 22:47:18 +0800

    April 1: Information - learned how to use spinner TODO: add spinner behavior. refer to this website: https://developer.android.com/guide/topics/ui/controls/spinner.html

--- app/src/main/java/com/example/deenma/chef/activities/InformationActivity.java
+++ app/src/main/java/com/example/deenma/chef/activities/InformationActivity.java
@@ -2,6 +2,8 @@ package com.example.deenma.chef.activities;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import com.example.deenma.chef.R;
 
@@ -14,5 +16,20 @@ public class InformationActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_information);
+        setupUI();
+    }
+
+    private void setupUI() {
+        // highway quest
+        Spinner spinnerHighwayQuest = (Spinner) findViewById(R.id.information_spinner_highway_quest);
+        ArrayAdapter<CharSequence> adapterHighwayQuest = ArrayAdapter.createFromResource(this, R.array.highway_options, android.R.layout.simple_spinner_item);
+        adapterHighwayQuest.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerHighwayQuest.setAdapter(adapterHighwayQuest);
+
+        // your information - plate color
+        Spinner spinnerYourInformationPlateColor = (Spinner) findViewById(R.id.information_spinner_your_information_plate_color);
+        ArrayAdapter<CharSequence> adapterYourInformationPlateColor = ArrayAdapter.createFromResource(this, R.array.plate_colors, android.R.layout.simple_spinner_item);
+        adapterYourInformationPlateColor.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerYourInformationPlateColor.setAdapter(adapterYourInformationPlateColor);
     }
 }
